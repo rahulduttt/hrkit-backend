4acd3198494a109e31361d2a40d423db7467a4d0
Rahul Dutt <d92.rahul@gmail.com>
2022-12-21 00:53:06 +1200

    Hello World!

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, &migratecmd.Options{
+		Automigrate: true, // auto creates migration files when making collection changes
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
